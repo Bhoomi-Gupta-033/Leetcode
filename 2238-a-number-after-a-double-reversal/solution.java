4c92eda3ffdc80032e0be678f39a2c47814b8cd0
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2025-11-25 19:50:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (50.57%)

---
+++ 2238-a-number-after-a-double-reversal/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        int rem = num%10;
+        if(num == 0){
+            return true;
+        }
+        if(rem == 0){
+             return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
