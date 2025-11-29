8bef19409787ab75de702bc08224579e91d44ffb
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2025-11-29 20:52:43 +0000

    [LeetCode Sync] Runtime - 1 ms (99.98%), Memory - 42.6 MB (63.10%)

--- 0007-reverse-integer/solution.java
+++ 0007-reverse-integer/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int reverse(int x) {
 
-            int n = -1 *x;
+            //int n = -1 *x;
+            int n =x;
             long rev = 0;
             while( n!= 0 ){
                 int rem = n%10;
                 rev = rev *10 + rem;
                 n = n/10;
             }
-            long nrev = -1 * rev;
-            if(nrev > Integer.MAX_VALUE || nrev < Integer.  MIN_VALUE) {
+            //long nrev = -1 * rev;
+            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
             return 0;
         } 
-            return( int)nrev;
+            return( int)rev;
         }
     }
 
