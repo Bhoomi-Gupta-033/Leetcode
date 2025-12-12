b6b4712c4d6966a9e54f9c09557893c930a27120
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2025-12-12 21:40:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (5.29%)

---
+++ 1293-three-consecutive-odds/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0; i<arr.length-2 ; i++){
+             int n1 = arr[i];
+             int n2 = arr[i+1];
+             int n3 = arr[i+2];
+             if(n1%2 != 0 && n2%2 != 0 && n3%2 !=0){
+                return true;
+             }
+        }
+        return false;
+    }
+}
