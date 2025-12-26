65be64557ce9120c4ac965933211386029e3e30a
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2025-12-26 08:18:57 +0000

    [LeetCode Sync] Runtime - 6 ms (22.32%), Memory - 45.9 MB (8.61%)

---
+++ 2106-find-greatest-common-divisor-of-array/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int max = nums[n-1];
+        int min = nums[0];
+         
+        int gcd = 1;
+        for(int i= min; i>=0; i--){
+            if(max%i == 0 && min %i == 0){
+                gcd = i;
+                break;
+            }
+        }
+
+        return gcd;
+    }
+}
