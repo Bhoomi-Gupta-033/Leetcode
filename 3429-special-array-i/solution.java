3ee7436db7e59b6e1221970f7a8dfaaf5b4c5ee5
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-01-10 08:31:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.4 MB (15.82%)

--- 3429-special-array-i/solution.java
+++ 3429-special-array-i/solution.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        for(int i =0 ; i<nums.length-1; i++){
-            if(nums[i] %2 == 0 && nums[i+1] %2 == 0 ||
-               nums[i] %2 != 0 && nums[i+1] %2 != 0 ){
-                       return false;
-               }
-        }
-        return true;
+       int n  = nums.length;
+
+       for(int i =0 ; i<n-1 ; i++){
+        int a = nums[i];
+        int b = nums[i+1];
+         if( a%2 == 0 && b%2 == 0 || 
+             a%2 != 0 && b%2 != 0){
+                return false;
+             }
+       }
+       return true;
     }
 }
