1776a595a78b04ddeb2c756be98fb7a1cf527815
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-02 08:03:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (78.90%)

--- 0202-happy-number/solution.java
+++ 0202-happy-number/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
-         while(n != 1){
-          int sum =0;
-             while(n !=0){
-            int rem = n%10;
-            sum = sum + (rem *rem);
-            n = n/10;
-            
-      }
-       n = sum;  
-       if(n == 2 || n==3 || n==4 || n==5 || n==6 || n==8 || n==9){
+         if(n == 1 || n== 7){
+            return true;
+         }  
+         else if ( n < 10){
             return false;
-        }     
-      }
-         
-         return true;    
+         }
+
+         int sum = 0; 
+         while ( n != 0){
+            int rem =  n %10;
+            sum = sum + rem * rem;
+            n = n/10;
+         }
+        
+         return isHappy(sum);
     }
 }
