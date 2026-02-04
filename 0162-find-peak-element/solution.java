c4d12cce116a69f85b734b8bd84e8ef2f4c375f9
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-04 19:35:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (5.22%)

--- 0162-find-peak-element/solution.java
+++ 0162-find-peak-element/solution.java
@@ -2,14 +2,13 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int max = nums[0];
         int max2 = 0;
-        int n = nums.length;
-
-        for(int i =0; i<n ; i++){
+        for(int i=0; i<nums.length; i++){
             if(nums[i] > max){
                 max = nums[i];
                 max2 = i;
             }
         }
+
         return max2;
     }
-}
+} 
