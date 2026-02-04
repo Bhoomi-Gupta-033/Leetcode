8777871f98a1f491a3fec28478af31a28f51a00d
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-04 19:29:20 +0000

    [LeetCode Sync] Runtime - 8 ms (15.89%), Memory - 48.1 MB (5.40%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,12 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
         int sum = 0;
-        for(int i = 0 ; i<nums.length ; i++){
+        for(int i=0;i<nums.length;i++){
             sum = sum + nums[i];
-        }
+        } 
 
-        int n = ((nums.length)*(nums.length+1))/2;
+         int n = nums.length;
+         int sumOfn =( n * (n+1))/2;
 
-        return n-sum;
+         return sumOfn - sum;
+        
     }
 }
