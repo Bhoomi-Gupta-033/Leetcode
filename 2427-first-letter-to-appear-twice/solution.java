2f22b48124dcbb6aeb1ad1ec306ce78575120b46
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-04 20:08:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (99.32%)

--- 2427-first-letter-to-appear-twice/solution.java
+++ 2427-first-letter-to-appear-twice/solution.java
@@ -2,15 +2,16 @@ class Solution {
     public char repeatedCharacter(String s) {
 
         HashSet<Character> set = new HashSet<>();
+           
+           for(int i = 0; i<s.length(); i++){
+             char ch = s.charAt(i);
+             if(set.contains(ch)){
+                return ch;
+             }else{
+                set.add(ch);
+             }
+           }
 
-        for(int i =0 ; i<s.length() ; i++){
-            if(set.contains(s.charAt(i))){
-                return s.charAt(i);
-            }
-            else{
-                set.add(s.charAt(i));
-            }
-        } 
-       return ' ';
+          return s.charAt(0);
     }
 }
