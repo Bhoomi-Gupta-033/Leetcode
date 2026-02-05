f26b56a63700b6fbab0e203e66a9c1a41938de86
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 22:13:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (69.30%)

--- 1585-the-kth-factor-of-n/solution.java
+++ 1585-the-kth-factor-of-n/solution.java
@@ -1,25 +1,17 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        int count = 0;
-    for(int  i =1; i<=n ; i++){
-        if(n%i == 0){
-           count++;
-        }      
-    }
+       int count= 1;
+       for(int i=1; i<=n; i++){
+           if(n%i == 0){
+              if( count != k){
+                count++;
+              }else{
+                return i;
+              }
+           }
+       }
 
-    int[] arr = new int[count];
-    int j =0;
-    for(int i =1 ; i<=n; i++){
-        if(n%i == 0){
-            arr[j] = i;
-            j++;
-        }
-    }
-if(arr.length >=k ){
-    return arr[k-1];
-}
-   return -1; 
-
-} 
+       return -1;
+    } 
 }  
 
