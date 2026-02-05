c925e8c6cbad7a68a485ef035131c6ba3e7cb3d2
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 02:55:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.2 MB (12.08%)

--- 0035-search-insert-position/solution.java
+++ 0035-search-insert-position/solution.java
@@ -1,23 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
-       int n = nums.length;
-       int low = 0;
-       int high = n-1;
-       
-       while(low<=high){
-           int mid = (low+high) /2;
+         
+         int low = 0;
+         int high = nums.length-1;
 
-           if(nums[mid] == target){
-            return mid;
-           }
-           else if( nums[mid] < target){
-            low = mid+1;
-           }else{
-            high = mid-1;
-           }
+         while(low<=high){
+            int mid = (low + high)/2;
 
-       }
-          return low;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+         }
+
+         return low;
     }
 }
