27de7814d23d165d59a39087e0be59009ac56844
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 06:27:05 +0000

    [LeetCode Sync] Runtime - 6 ms (17.02%), Memory - 42.2 MB (47.89%)

--- 0633-sum-of-square-numbers/solution.java
+++ 0633-sum-of-square-numbers/solution.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long a =0;
+      long a = 0;
       long b = 0;
 
-      while( a*a <=c - b*b){
+      while( a*a < c- b*b){
         a++;
       }
-      for(long i =0; i<=a ;i++){
-       long ans = c- i*i;
-       double f = Math.pow(ans, 0.5);
 
-       if(f == (int)f){
-       return true;
-       }
+      for(int i = 0; i <=a ; i++){
+         long ans = c- i*i;
+         double root = Math.pow(ans , 0.5);
+
+         if(root == (int)root){
+            return true;
+         }
+        
       }
       return false;
 }
