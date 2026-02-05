5bde5a42fac8b1dbf5cd27393496b53944134753
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 19:14:16 +0000

    [LeetCode Sync] Runtime - 2149 ms (5.05%), Memory - 42.4 MB (9.69%)

--- 0507-perfect-number/solution.java
+++ 0507-perfect-number/solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int sum = 0;
-        for(int i = 1; i<num ; i++){
+
+        for(int i =1; i<num; i++){
             if(num%i == 0){
-               sum = sum +i;
-            }
+                sum = sum + i;
+            } 
         }
-        if(sum == num){
+
+        if( sum == num){
             return true;
         }
-        return false;
+        else{
+            return false;
+        }
     }
 }
