01166abb7cb1d4eb5b91c5a469e4845516e27401
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 03:56:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.8 MB (40.99%)

--- 0812-rotate-string/solution.java
+++ 0812-rotate-string/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-           if(s.length() != goal.length()){
+           if(s.length() != goal.length()) {
             return false;
            }
            else{
-            String res = goal + goal;
-           return res.contains(s); 
+            String n = goal + goal;
+            return n.contains(s);
            }
     }
 
