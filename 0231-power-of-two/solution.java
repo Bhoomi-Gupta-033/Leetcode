33628e41f85242a769607ba46180a9697e160364
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 21:38:45 +0000

    [LeetCode Sync] Runtime - 1 ms (96.40%), Memory - 42.5 MB (71.94%)

--- 0231-power-of-two/solution.java
+++ 0231-power-of-two/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         
-        if( n == 0){
-            return false;
-        }
-        else{
+       if(n == 0) return false;
+       else if(n==1) return true;
 
-        while(n != 1){
-          int rem = n%2;
-
-          if(rem != 0){
-            return false;
-          }
-          n = n/2;
-        }
+       while( n != 1){
+             
+             int rem = n%2;
+            if( rem != 0){
+                return false;
+            } 
+            n = n/2;
+       }
         return true;
-        }
-        
     }
 }
