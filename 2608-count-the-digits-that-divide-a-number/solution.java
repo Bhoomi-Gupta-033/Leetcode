ef67a1baecc15f907cdcb56e6d09ee33adb8f7ea
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 18:45:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (17.75%)

--- 2608-count-the-digits-that-divide-a-number/solution.java
+++ 2608-count-the-digits-that-divide-a-number/solution.java
@@ -1,14 +1,19 @@
 class Solution {
     public int countDigits(int num) {
-        int dummy = num;
-        int count = 0;
-        while(dummy != 0){
-            int rem = dummy%10;
-            if(num%rem == 0){
-                count =  count+1;
-            }
-            dummy =dummy/10;
+        if( num <10){
+            return 1;
         }
+
+        int count = 0;
+        int dummy = num;
+        while( num != 0 ){
+            int rem = num %10;
+            if(dummy %rem == 0){
+                count++;
+            }
+            num = num /10;
+        }
+
         return count;
     }
 }
