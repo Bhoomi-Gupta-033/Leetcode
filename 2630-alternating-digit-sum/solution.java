a960f2819ca5fbb7624aea73402948bf6c64a463
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 17:58:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (12.55%)

--- 2630-alternating-digit-sum/solution.java
+++ 2630-alternating-digit-sum/solution.java
@@ -1,27 +1,26 @@
 class Solution {
     public int alternateDigitSum(int n) {
-       int sum = 0;
-    int count = 0;
-    int dummy = n;
 
-     while(n != 0){
-         count  = count + 1;
-         n = n/10;
-     }
+      int digits = 0;
+      int sum = 0;
+      int dummy = n;       
+             while(n !=0){
+                digits++;
+                n = n/10;   
+             }
 
-     while( dummy != 0){
-         int rem = dummy%10;
-         if(count %2 == 0){
-            sum = sum - rem;
-            count--;
-         }
-         else{
-            sum = sum + rem;
-            count--;
-         }
-         dummy = dummy/10;
-     }
-      return (sum);
-        
+         while( dummy != 0){
+            int rem = dummy%10;
+            if(digits%2 == 0){
+                sum = sum -rem;
+                digits--;
+            }else{
+                sum = sum + rem;
+                digits--;
+            }
+
+            dummy = dummy/10;
+         }    
+             return sum;
     }
 }
