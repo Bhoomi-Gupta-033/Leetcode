46911f1a233bcee340bb79ba1259589d90cbebfd
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 03:07:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.6 MB (30.74%)

--- 0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int profit =0;
-
-        for(int i = 0; i<n-1 ; i++){
+        int profit = 0;
+        
+        for(int i = 0; i < prices.length-1; i++){
             if(prices[i] < prices[i+1]){
-               profit = profit + (prices[i+1] - prices[i]);
+                profit= profit + (prices[i+1] - prices[i]);
             }
-            
         }
+
         return profit;
     }
 }
