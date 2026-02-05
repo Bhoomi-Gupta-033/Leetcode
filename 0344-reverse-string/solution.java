08e968571b37733350401a41283e76781e20e8e1
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 05:58:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.4 MB (20.89%)

--- 0344-reverse-string/solution.java
+++ 0344-reverse-string/solution.java
@@ -1,18 +1,20 @@
 class Solution {
     public void reverseString(char[] s) {
+       
        int n = s.length;
-       int low = 0;
+       int low =0;
        int high = n-1;
-       while(low<high){
-        char temp = s[low];
-        s[low] = s[high];
-        s[high] = temp;
-        low++;
-        high--;
+
+       while(low < high){
+           
+           char temp = s[low];
+           s[low] = s[high];
+           s[high] = temp;
+
+           low++;
+           high--;
+
        }
 
-       for(int i =0 ; i<n ; i++){
-         System.out.print(s[i]);
-       }
     }
 }
