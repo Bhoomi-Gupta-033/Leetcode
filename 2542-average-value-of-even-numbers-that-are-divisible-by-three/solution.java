c98a88f7e01f7877666154ec6ad4017dc0ca3e1d
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 18:51:44 +0000

    [LeetCode Sync] Runtime - 1 ms (69.62%), Memory - 46.4 MB (36.35%)

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/solution.java
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/solution.java
@@ -1,20 +1,22 @@
 class Solution {
     public int averageValue(int[] nums) {
-        int sum =0;
-        int count = 0;
-        for(int i = 0 ; i<nums.length; i++){
-             if(nums[i]%2 == 0){
-                if(nums[i]%3 == 0){
-                    sum = sum + nums[i];
-                    count++;
+          int sum = 0;
+          int count = 0;
+
+            for(int i = 0; i < nums.length ; i++){
+                if(nums[i] %2 == 0){
+                    if(nums[i] %3 == 0){
+                        sum = sum + nums[i];
+                        count++;
+                    }
                 }
-             }
-        }
-        if(count==0){
-            return 0;
-        }
-        else{
-            return sum/count;
+            }
+            if(count != 0){
+                int ans = sum/count;
+                return ans;
+            }else{
+                return 0;
+            }      
+            
         }
     }
-}
