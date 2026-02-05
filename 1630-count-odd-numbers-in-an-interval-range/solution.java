b174dab8799291b506054b29a3bde88a990d0b61
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 19:00:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (6.29%)

--- 1630-count-odd-numbers-in-an-interval-range/solution.java
+++ 1630-count-odd-numbers-in-an-interval-range/solution.java
@@ -1,15 +1,5 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int count = 0;
-        if (low % 2 == 0) {
-          low++;
-           }
-        for(int i= low; i<=high ;i= i+2){
-            if(i%2 != 0){
-                count = count +1;
-            }
-           
-        }
-        return count;
+        return (high + 1) / 2 - low / 2;
     }
 }
