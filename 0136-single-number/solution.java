7a1d6a9b4e408dbf30065ea806df2ef143460971
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-05 03:39:33 +0000

    [LeetCode Sync] Runtime - 10 ms (26.64%), Memory - 47 MB (45.31%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        int ans= 0;
-        for(int i = 0 ; i<n; i=i+1){
-           ans = ans ^ nums[i];
-        }
-        return ans;
+       Arrays.sort(nums);
+
+       for(int i = 0 ; i<nums.length-1; i = i+2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+       }
+
+       return nums[nums.length -1];
     }
 }
