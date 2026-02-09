92f6ea1cdc060d1d60258cc54f668d5769429f2c
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-09 08:40:44 +0000

    [LeetCode Sync] Runtime - 3 ms (92.44%), Memory - 46.2 MB (48.19%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,26 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         String ans = "";
-    
-    if(s.length() == t.length()){
-       for(int i =0 ; i<s.length(); i++){
-            char ch = s.charAt(i);
-            for(int j =0 ;j< t.length();j++){
-                char sh = t.charAt(j);
-                if(ch == sh){
-                     ans = ans + sh;
-                      t = t.substring(0, j) + t.substring(j + 1);
-                     break;
-                }
-                
-            }
-        }
-        
-        return (s.equals(ans));
-    }
-    else{
-      return false;
-    }
+          if(s.length() != t.length()) return false;
+
+          char[] ch1 = s.toCharArray();
+          char[] ch2 = t.toCharArray();
+
+          Arrays.sort(ch1);
+          Arrays.sort(ch2);
+
+           return Arrays.equals(ch1 , ch2);
        
     }
 }
