f61b3b2cabb5430d881b3d2e2072568577febe82
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-09 08:06:11 +0000

    [LeetCode Sync] Runtime - 13 ms (30.02%), Memory - 52.5 MB (90.62%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,8 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-
-        return nums[(n/2)];
+      int n = nums.length;
+      HashMap<Integer , Integer> map = new HashMap<>();
+      for(int i=0; i<nums.length;i++){
+          map.put(nums[i] , map.getOrDefault(nums[i] , 0) + 1);
+      }
+      
+      for(int el : map.keySet()){
+        
+        if(map.get(el) > n/2){
+          
+          return el;
+        }
+      }
+      
+     return 0;
     }
 }
