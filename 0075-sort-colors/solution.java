97baa43b0af38a6390df2f8705ea6b72c6329267
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-10 10:04:31 +0000

    [LeetCode Sync] Runtime - 1 ms (21.41%), Memory - 43.8 MB (10.14%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -2,16 +2,36 @@ class Solution {
     public void sortColors(int[] nums) {
        
        int n = nums.length;
+       int[] res = new int[n];
+       HashMap<Integer , Integer> map = new HashMap<>();
+       
+       for(int i =0; i<n;i++){
+        map.put(nums[i] , map.getOrDefault(nums[i] , 0) +1);
+       }
+       
+       int c0 =0;
+       int c1 =0;
 
-        for(int j = 0; j<n ; j++){
-            for(int i = 0; i<n-1 ;i++){
-            if(nums[i] > nums[i+1]){
-                int temp = nums[i];
-                nums[i] = nums[i+1];
-                nums[i+1] = temp;
-            }
-        }
-        }
+       for(int el : map.keySet()){
+          if(map.get(0) != null){
+             c0 = map.get(0);
+         }
+         if(map.get(1) != null){
+            c1 = map.get(1);
+         }
+       }
+ 
+       for(int i=0;i<c0;i++){
+        nums[i] = 0;
+       }
+       for(int i=c0;i<(c0+c1);i++){
+          nums[i] = 1;
+       }
+
+       for(int i=(c0+c1);i<n;i++){
+          nums[i] = 2;
+       }
+        
    }
 
 }
