69a866096a9ea674708641865d5c4b89b53984c3
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-16 09:03:19 +0000

    [LeetCode Sync] Runtime - 20 ms (83.15%), Memory - 56.7 MB (9.34%)

---
+++ 0917-boats-to-save-people/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+         int count =0;
+          
+          Arrays.sort(people);
+
+          int low =0;
+          int high = n-1;
+
+          while(low<=high){
+            if(people[low] + people[high] <= limit){
+                count++;
+                low++;
+                high--;
+            }else{
+                count++;
+                high--;
+            }
+          }
+
+          return count;
+    }
+}
