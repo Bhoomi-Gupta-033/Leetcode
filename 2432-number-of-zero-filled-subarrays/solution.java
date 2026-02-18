2061698cd48da77482a356565c505f13a3fb3616
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-18 03:11:16 +0000

    [LeetCode Sync] Runtime - 3 ms (97.07%), Memory - 115.7 MB (47.56%)

--- 2432-number-of-zero-filled-subarrays/solution.java
+++ 2432-number-of-zero-filled-subarrays/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        int n = nums.length; 
-        int count =0;
-        long ans = 0;
-        for(int i =0; i<n;i++){
-            if(nums[i] == 0){
+        int n = nums.length;
+        int count= 0;
+        long ans =0;
+
+        for(int i =0; i<n; i++){
+            if( nums[i] == 0){
                 count++;
                 ans = ans + count;
             }else{
-                count = 0;
+                count =0;
             }
-        }  
+        }
 
-       return ans;
+        return ans;
     }
 }
