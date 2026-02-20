a1fc58c0210f85ba86c0dddff4ed5da070522016
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-20 08:07:55 +0000

    [LeetCode Sync] Runtime - 4 ms (10.63%), Memory - 47.6 MB (12.94%)

--- 1704-special-positions-in-a-binary-matrix/solution.java
+++ 1704-special-positions-in-a-binary-matrix/solution.java
@@ -3,31 +3,31 @@ class Solution {
         int row = mat.length;
         int col = mat[0].length;
 
-        int[] rowCount = new int[row];
-        int[] colCount = new int[col];
-
-        // count 1s
+         int sp =0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (mat[i][j] == 1) {
-                    rowCount[i]++;
-                    colCount[j]++;
+                    
+                    int rsum = 0;
+                    int csum =0;
+                    for(int k =0; k<row; k++){
+                          rsum = rsum + mat[k][j];
+
+                    }
+
+                    for(int k =0; k<col; k++){
+                        csum = csum + mat[i][k];
+                    }
+
+                    if(rsum == 1 && csum == 1){
+                        sp++;
+                    }
+
                 }
             }
         }
 
-        int count = 0;
-
-        // check special positions
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+        return sp;
 
     }
 }
