f1f64f9f6d4e7d72215b1e057cb7ddf9906eca16
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-25 00:12:33 +0000

    [LeetCode Sync] Runtime - 3 ms (48.23%), Memory - 45.2 MB (25.65%)

--- 0349-intersection-of-two-arrays/solution.java
+++ 0349-intersection-of-two-arrays/solution.java
@@ -1,30 +1,37 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        int n =  nums1.length;
+        int m =  nums2.length;
+
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
         
-       ArrayList<Integer> arrlist = new ArrayList<>();
-       int count= 0;
-        for(int i=0 ; i<nums1.length;i++){
-             int a = nums1[i];
-
-             for(int  j=0 ; j<nums2.length; j++){
-              int b = nums2[j];
-               
-               if(a == b){
-                if (!arrlist.contains(a)){ 
-                    arrlist.add(a);
-                    count++;
-                }
-                    
-               }
-
-             }
+        for(int i =0; i<n;i++){
+            set1.add(nums1[i]);
         }
 
-        int[] arr = new int[count];
-        for(int i=0 ; i<arr.length;i++){
-             arr[i] = arrlist.get(i);
+        for(int i =0; i<m;i++){
+            set2.add(nums2[i]);
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int count =0;
+        for(int i : set1){
+            if(set2.contains(i)){
+                list.add(i);
+                count++;
+            }
         }
 
-        return arr;
+        int[] ans = new int[count];
+        
+        int i =0;
+        for(int el : list){
+            ans[i] = el;
+            i++;
+        }
+
+        return ans;
     }
 }
