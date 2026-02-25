345dbaef3a232c0dc8ad8e04e8d77cd10ea39ad5
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-25 00:01:36 +0000

    [LeetCode Sync] Runtime - 1 ms (93.79%), Memory - 42.7 MB (95.77%)

--- 2436-make-array-zero-by-subtracting-equal-amounts/solution.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-         Arrays.sort(nums);
-         HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
 
-         for(int i =0; i<nums.length ; i++){
-            if( nums[i] != 0){
-                if(!(set.contains(nums[i]))){
-               set.add(nums[i]);
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i =0; i<n;i++){
+            if(nums[i] != 0){
+                set.add(nums[i]);
             }
-            }
-         }
+        }
 
-         return set.size();
-
+        return set.size();
     }
 }
