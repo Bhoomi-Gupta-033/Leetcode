b9ae28780266c1c31913019d49d86def1a122779
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-26 19:55:09 +0000

    [LeetCode Sync] Runtime - 6 ms (25.72%), Memory - 49.2 MB (15.21%)

--- 0004-median-of-two-sorted-arrays/solution.java
+++ 0004-median-of-two-sorted-arrays/solution.java
@@ -1,38 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     
-      int n = nums1.length;
+        int n = nums1.length;
         int m = nums2.length;
 
-        int len = m+n;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        int[] res = new int[len];
-         
-         for(int i = 0; i<n ; i++){
-            res[i] = nums1[i];
-         }
+        for(int i = 0; i<n; i++){
+            list.add(nums1[i]);
+        }
+        for(int i = 0; i<m; i++){
+            list.add(nums2[i]);
+        }
 
-         int j = 0;
-         for(int i = n; i<m+n ; i++){
-            if( j < m){
-                res[i] = nums2[j];
-            j++;
-            }
-         }
-         Arrays.sort(res);
+        Collections.sort(list);
 
-         double ans = 0;
-         if(len%2 != 0){
-             ans =  res[((len)/2)];
-            
-         }else{
-                ans = (res[(len)/2 -1] + res[(len)/2])/2.0;
+        int s = list.size();
 
-         }
-         
-         return ans;
+        if( s%2 != 0){
+            double ans = list.get(s/2);
+            return ans;
+        }else{
+            double ans = (list.get((s/2) -1) + list.get(s/2))/2.0;
+            return ans;
+        }
 
-        
+     
     }
-
 }
