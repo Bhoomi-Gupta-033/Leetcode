75b26054a9545db16e4a47e3a7208403171be8ab
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-02-26 19:21:10 +0000

    [LeetCode Sync] Runtime - 37 ms (5.28%), Memory - 96.8 MB (16.74%)

--- 0217-contains-duplicate/solution.java
+++ 0217-contains-duplicate/solution.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        HashMap<Integer , Integer> map = new HashMap<>();
 
-       for(int i = 0; i<nums.length;i++){
-        if(!set.contains(nums[i])){
-            set.add(nums[i]);
-        }else{
-            return true;
+        for(int i =0; i<n;i++){
+            map.put(nums[i] , map.getOrDefault(nums[i] , 0) +1);
         }
-        
-       }
 
-       return false;
+        for(int key : map.keySet()){
+            if(map.get(key)>1){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
