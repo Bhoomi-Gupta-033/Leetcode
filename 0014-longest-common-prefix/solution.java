7f66345339f88952525f711e41540deca1cbcd1d
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-10 20:11:27 +0000

    [LeetCode Sync] Runtime - 4 ms (13.28%), Memory - 43.5 MB (8.47%)

--- 0014-longest-common-prefix/solution.java
+++ 0014-longest-common-prefix/solution.java
@@ -1,27 +1,22 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-         Arrays.sort(strs);
+        Arrays.sort(strs);
+        int n = strs.length; 
+        String s1 = strs[0];
+        String s2 = strs[n-1];
 
-         int n = strs.length;
-         String s1 = strs[0];
-         String s2 = strs[n-1];
+        String ans = "";
 
-         int n2 = s1.length();
-         int m = s2.length();
-
-         int len = Math.max(n2 , m);
-         
-         String res = "";
-         for(int i =0; i<s1.length(); i++){
-            
+        int i = 0;
+        while( i < s1.length()){
             if(s1.charAt(i) == s2.charAt(i)){
-                res = res + s1.charAt(i);
+                ans+=s1.charAt(i);
             }else{
                 break;
             }
-         }
-            
+            i++;
+        }
 
-            return res;
+        return ans;
     }
 }
