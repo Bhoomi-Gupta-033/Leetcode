57ccc25c4ff6f11b9fec55c0fdf92c007c8b4dd5
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-11 18:14:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (16.92%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int a =1;
-        int b =2;
-        int stair =0;
-        if( n==1 || n==2) return n;
-
-        for(int i = 3 ; i<= n ; i++){
-            stair = a+b;
-            a=b;
-            b=stair;
+        int a = 1;
+        int b = 2;
+        if(n == 1 || n==2) return n;
+        for(int i = 3; i<=n;i++){
+           int temp = a+b;
+           a = b;
+           b = temp;
         }
 
-        return stair;
+        return b;
     }
 }
