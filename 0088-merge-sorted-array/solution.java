591e105bfe0048d8fd5135eb19cdd1962c5a761c
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-11 18:01:41 +0000

    [LeetCode Sync] Runtime - 4 ms (15.82%), Memory - 43.9 MB (44.35%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -1,19 +1,11 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int j = 0;
-      for(int i=m;i<nums1.length ; i++){
-          nums1[i] = nums2[j];
-          j++;
-      }
-      
-      for(int k =0 ; k<nums1.length ; k++){
-        for(int i=0 ; i<nums1.length-1 ; i++){
-          if(nums1[i] > nums1[i+1]){
-              int temp = nums1[i];
-              nums1[i] = nums1[i+1];
-              nums1[i+1] = temp;
-          }
-      }
-      }
+        for(int i = m;i<nums1.length;i++){
+            nums1[i] = nums2[j];
+            j++;
+        }
+
+        Arrays.sort(nums1);
     }
 }
