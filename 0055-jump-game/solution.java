5359884cad60d1a46b3fc15ef922665bdba6d292
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-16 01:34:53 +0000

    [LeetCode Sync] Runtime - 2 ms (88.79%), Memory - 48.2 MB (8.95%)

---
+++ 0055-jump-game/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxReach  =0;
+         for(int i = 0; i<n; i++){   
+            maxReach = Math.max(maxReach , i + nums[i]); 
+
+            if(maxReach >= n-1){
+                return true;
+            }   
+
+            if(i == maxReach){
+                return false;
+            }
+         }   
+            
+            return false;
+    }
+}
