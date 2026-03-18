37bec150736ba67b2a32837af4f4916e2262dcb5
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-18 19:11:17 +0000

    [LeetCode Sync] Runtime - 2 ms (94.59%), Memory - 65.2 MB (70.25%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        
-        int[] pre = new int[n];
-        pre[0] = 1;
-         
-        for(int i =1; i<n;i++){
-            pre[i] = nums[i-1] * pre[i-1];
-        }
+       int n = nums.length;
+       int[] pre = new int[n];
+       int[] suf = new int[n];
 
-        int[] suf = new int[n];
-        suf[n-1] = 1;
-         
-        for(int i=n-2; i>=0;i--){
-            suf[i] = nums[i+1] * suf[i+1];
-        }
+       pre[0] = 1;
+       suf[n-1] = 1;
 
-        int[] ans = new int[n];
-        for(int i=0; i<n; i++){
-            ans[i] = pre[i] * suf[i];
-        }
+       for(int i = 0; i<n-1;i++){
+         pre[i+1] = pre[i]*nums[i];
+       }
 
-        return ans;
+       for(int i = n-1; i>0; i--){
+         suf[i-1] = suf[i] * nums[i];
+       }
+
+       int[] ans = new int[n];
+
+       for(int i = 0; i<n; i++){
+        ans[i] = pre[i] * suf[i];
+       }
+
+       return ans;
     }
 }
