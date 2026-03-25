f62a044ec9a8bb3561cc42a68ce29ede4ed5d8e4
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-25 01:41:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (82.38%)

--- 0782-jewels-and-stones/solution.java
+++ 0782-jewels-and-stones/solution.java
@@ -1,15 +1,18 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-         int count = 0;
-      for(int i=0; i<stones.length();i++){
-        char ch = stones.charAt(i);
-        for(int j =0 ; j<jewels.length(); j++){
-          char ch1 = jewels.charAt(j);
-          if(ch == ch1){
-             count = count + 1;
-          }
+        int count= 0;
+        for(int i =0; i<jewels.length();i++){
+            char ch = jewels.charAt(i);
+
+            for(int j =0; j<stones.length(); j++){
+                char sh = stones.charAt(j);
+
+                if(sh == ch) count++;
+            }
         }
-      }
-      return count;
-    }
+        
+        
+        return count;
+        
+            }
 }
