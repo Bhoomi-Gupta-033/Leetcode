734ba80e67873c0167ab7b0c9b0a3e930f0625de
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-25 02:07:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (6.31%)

--- 2507-number-of-common-factors/solution.java
+++ 2507-number-of-common-factors/solution.java
@@ -1,12 +1,11 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int c= Math.min(a,b);
-        int count = 0;
-        for(int i = 1; i<=c ; i++){
-            if(a%i == 0 && b %i == 0){
-                count = count +1;
-            }
-        }
-        return count;
+         int min = Math.min(a , b);
+          int count = 0;
+         for(int i = 1;i<=min; i++){
+           if(a%i == 0 && b%i == 0) count++;
+         }
+
+         return count;
     }
 }
