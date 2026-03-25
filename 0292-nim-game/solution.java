87be3a623c6d0f93bb971426b0a47bcc3e007f62
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-25 02:19:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (93.63%)

--- 0292-nim-game/solution.java
+++ 0292-nim-game/solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if (n<4){
-            return true;
-        }
-        else if( n%4 == 0){
+        if(n%4 == 0){
             return false;
         }
-        else{
-            return true;
-        }
+
+        return true;
     }
 }
