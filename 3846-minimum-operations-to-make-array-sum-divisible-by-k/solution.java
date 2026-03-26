d2582e1c391933a5cd4081f921b8d6ea8205e39b
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-26 03:34:28 +0000

    [LeetCode Sync] Runtime - 1 ms (97.89%), Memory - 46.6 MB (94.16%)

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/solution.java
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/solution.java
@@ -1,35 +1,11 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int sum =0;
-        int max = 0;
-        int count = 0;
-        for(int i =0 ;i<nums.length;i++){
-            int n = nums[i];
-            sum = sum +n;
-            if(n>max){
-                max =n;
-            }
-        }
-        //System.out.println(sum);
-        //System.out.println(max);
+        int sum = 0;
 
-        if(sum<k){
-          //System.out.println(sum);
-          return sum;
+        for(int i =0; i<nums.length;i++){
+            sum = sum + nums[i];
         }
-        else if (sum %k == 0){
-           //System.out.println(0);
-           return 0;
-        }
-        else{
-            while(sum%k !=0){
-              count++;
-              sum = sum -max;
-              max= max -1;
-              sum = sum +max;
-            }
-            //System.out.println(count);
-            return count;
-        }
+
+        return sum%k;
     }
 }
