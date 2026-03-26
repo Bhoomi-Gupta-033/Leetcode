4ba8cbe93deb5c0909dc422adaa06ee5231b4887
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-26 04:19:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.1 MB (74.60%)

--- 1950-sign-of-the-product-of-an-array/solution.java
+++ 1950-sign-of-the-product-of-an-array/solution.java
@@ -1,27 +1,20 @@
 class Solution {
     public int arraySign(int[] nums) {
-        //int signFunc(product) =1;
+        int neg = 0;
         
-        int countN =0;
-        int count = 0;
-         for(int i=0; i<nums.length; i++){
-            if(nums[i] <0){
-                countN++;
+        for(int i = 0; i<nums.length; i++){
+            int a = nums[i];
+             if(a<0){
+                neg++;
+            }else if(a == 0){
+                return 0;
             }
-            else if(nums[i] == 0){
-                count++;
-            }
-         }
-         if(count>=1){
-            return 0;
-         }
-         else if(countN%2 == 0){
-            return 1;
-         }
-         else{
-            return -1;
-         }
+        }
+
+        if(neg%2 == 0){
+           return 1;
+        }
+
+        return -1;
     }
 }
-
-
