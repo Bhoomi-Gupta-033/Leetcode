acc8a15c13ea2098bd2b2fd36a9aa8f07bc99559
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-26 03:45:09 +0000

    [LeetCode Sync] Runtime - 1 ms (90.42%), Memory - 43.3 MB (37.28%)

--- 1781-check-if-two-string-arrays-are-equivalent/solution.java
+++ 1781-check-if-two-string-arrays-are-equivalent/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String wordO ="";
-        for(int i = 0; i<word1.length;i++){
-    String s = word1[i];
-    wordO= wordO+ s;
-}
+        String str1 = "";
 
-     String wordT ="";
-     for(int i = 0; i<word2.length;i++){
-     String s = word2[i];
-     wordT = wordT + s;
-}
-     return wordT.equals(wordO);
-        
+        for(int i = 0; i<word1.length; i++){
+            str1+=word1[i];
+        }
+         
+         String str2 = "";
+
+        for(int i = 0; i<word2.length; i++){
+            str2+=word2[i];
+        }
+
+        return str1.equals(str2);
+
     }
 }
