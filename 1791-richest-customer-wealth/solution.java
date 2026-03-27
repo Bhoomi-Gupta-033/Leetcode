92b5015be826769718c903e4c6231b1583185473
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-27 04:56:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.8 MB (18.31%)

--- 1791-richest-customer-wealth/solution.java
+++ 1791-richest-customer-wealth/solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        
-        int max = 0;
-        for(int i =0; i<accounts.length; i++){
-            int[] arr = accounts[i];
-            
+        int row = accounts.length;
+        int col = accounts[0].length;
+
+       int max = 0;
+        for(int i = 0; i<row;i++){
             int sum = 0;
-            for(int j = 0 ; j<arr.length ; j++){
-                int n = arr[j];
-                sum = sum + n;
+            for(int j = 0; j<col; j++){
+              sum+=accounts[i][j];
             }
-            if(sum >max){
-                max = sum;
-            }
-            
-           
-        }
+             
+             max = Math.max(sum , max);
+
+        } 
+
         return max;
     }
 }
