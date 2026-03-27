bf18e9d476c2035762e4c541512d67bdb494027a
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-27 20:23:31 +0000

    [LeetCode Sync] Runtime - 5 ms (33.27%), Memory - 47.4 MB (6.41%)

--- 0013-roman-to-integer/solution.java
+++ 0013-roman-to-integer/solution.java
@@ -1,34 +1,36 @@
 class Solution {
     public int romanToInt(String s) {
         int n = s.length();
+
+        HashMap<Character , Integer> map = new HashMap<>();
+        map.put('I' , 1); 
+        map.put('V' , 5); 
+        map.put('X' , 10); 
+        map.put('L' , 50); 
+        map.put('C' , 100); 
+        map.put('D' , 500); 
+        map.put('M' , 1000);   
+        
         int sum = 0;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        for (int i = 0; i < n - 1;) {
-            if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1))) {
-                sum += map.get(s.charAt(i));
-                i++;
-            } else {
-                int add = map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
-                sum += add;
-                i += 2;
+        for(int i=0; i<n-1;){
+            
+            if(map.get(s.charAt(i)) >= map.get(s.charAt(i+1))){
+               sum+=map.get(s.charAt(i));
+               i++;
+            }else if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+              sum+=(map.get(s.charAt(i+1)) - map.get(s.charAt(i)));
+               i+=2;
             }
+
         }
 
-        if ( n>1 && map.get(s.charAt(n - 1)) <= map.get(s.charAt(n -          2))) {
-            sum += map.get(s.charAt(n - 1));
-        }else if(n == 1){
-            sum =  map.get(s.charAt(0));   
+        if(n>1 && map.get(s.charAt(n-1)) <= map.get(s.charAt(n-2))) {
+            sum+=map.get(s.charAt(n-1));
+        }else if (n == 1){
+              sum+=map.get(s.charAt(0));
         }
-
-        return sum;
+        
+         
+         return sum;
     }
 }
