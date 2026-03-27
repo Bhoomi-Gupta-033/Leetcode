5521b29680bd0fa13c41e588a1b8a9a3ee9998bf
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-27 20:40:55 +0000

    [LeetCode Sync] Runtime - 1526 ms (23.12%), Memory - 48.6 MB (78.96%)

--- 0560-subarray-sum-equals-k/solution.java
+++ 0560-subarray-sum-equals-k/solution.java
@@ -1,18 +1,22 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-           int n = nums.length;
-         
-         int count =0;
-         
-        for(int i =0; i<n;i++){
-            int sum = 0;
-           for(int j = i; j<n;j++){
-            sum+=nums[j];
-            if(sum==k){
-                count++;
-            }
+        int n = nums.length;
+        int count = 0;
+        for(int i =0; i<n; i++){
+            int sum = nums[i];
+          if(nums[i] == k){
+            count++;
+          }
+
+          for(int j=i+1;j<n;j++){
+                sum+=nums[j];
+
+                if(sum == k){
+                    count++;
+                }
           }
         }
+
         return count;
     }
 }
