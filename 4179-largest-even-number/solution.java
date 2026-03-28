a56ee406c66e94d9eb7965ca73152c457d376960
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-28 20:38:58 +0000

    [LeetCode Sync] Runtime - 1 ms (63.55%), Memory - 43.8 MB (88.55%)

--- 4179-largest-even-number/solution.java
+++ 4179-largest-even-number/solution.java
@@ -1,13 +1,18 @@
 class Solution {
     public String largestEven(String s) {
-        int n = s.length();
-        for(int i = n-1 ; i>=0 ; i--){
-            char ch = s.charAt(i);
-           int num = ch - '0';
-            if(num %2 == 0){
-                return s.substring(0 , i+1);
-            }
+        // String ans = "";
+        int n  = s.length();
+
+        for(int i=n-1; i>=0; i--){
+         int a = s.charAt(i) - '0';
+
+         if(a%2 != 0){
+            s = s.substring(0 , i);
+         }else{
+            break;
+         }
         }
-    return "";
+
+        return s;
     }
 }
