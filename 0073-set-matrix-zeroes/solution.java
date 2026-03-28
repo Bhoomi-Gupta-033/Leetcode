7827cbf0deb28507100ed48e77e61e43b3189e94
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-28 23:10:43 +0000

    [LeetCode Sync] Runtime - 17 ms (5.27%), Memory - 47.6 MB (61.55%)

--- 0073-set-matrix-zeroes/solution.java
+++ 0073-set-matrix-zeroes/solution.java
@@ -1,32 +1,31 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-              
-          HashSet<Integer> rowSet = new HashSet<>();
+        HashSet<Integer> rowSet = new HashSet<>();
         HashSet<Integer> colSet = new HashSet<>();
 
+          int row = matrix.length;
+          int col = matrix[0].length;
         for(int i =0; i<row; i++){
             for(int j =0; j<col; j++){
-                 if(matrix[i][j] == 0){
-                rowSet.add(i);
-                colSet.add(j);
-              }
-                
+                if(matrix[i][j] == 0){
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
             }
         }
          
-         for(int r : rowSet){
-              for(int c : colSet){
-                for(int i =0; i<row; i++){
-                  matrix[i][c] = 0;
-                }
-                for(int i =0; i<col; i++){
-                  matrix[r][i] = 0;
-                }
-              }
+        for(int rowS : rowSet){
+            for(int colS : colSet){
+                    for(int i =0; i<col; i++){
+                        matrix[rowS][i] = 0;
+                    }
+
+                    for(int i =0; i<row; i++){
+                        matrix[i][colS] = 0;
+                    }
             }
+        }
 
-
+        
     }
 }
