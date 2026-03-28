6740e67d53ccdf231bfc6d13625e8da4f1ffb228
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-28 21:57:22 +0000

    [LeetCode Sync] Runtime - 5 ms (64.85%), Memory - 46.7 MB (5.20%)

--- 1636-number-of-substrings-with-only-1s/solution.java
+++ 1636-number-of-substrings-with-only-1s/solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public int numSub(String s) {
-        
-        int count = 0;
-        int ans  = 0;
+        int n= s.length();
 
-        for(int i =0; i<s.length(); i++){
+        int count = 0;
+        int max = 0;
+
+        for(int i = 0; i<n; i++){
             if(s.charAt(i) == '1'){
                 count++;
             }else{
                 count = 0;
             }
 
-            ans = (ans+count)% 1000000007;
+            max =( max + count)% 1000000007;
+
         }
-        
-        return ans;
+
+           
+        return max ;
     }
 }
