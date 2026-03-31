1ce0a27c41b6246de884c436176e1e6a8788d89b
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-03-31 10:18:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.2 MB (23.01%)

--- 0054-spiral-matrix/solution.java
+++ 0054-spiral-matrix/solution.java
@@ -3,33 +3,34 @@ class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
 
-        List<Integer> list = new ArrayList<>();
-
         int tr = 0;
         int br = row-1;
-        int lc = 0;
         int rc = col-1;
+        int lc = 0;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        while(tr <=br && lc <= rc){
-            for(int i=lc; i<=rc; i++){
-                list.add(matrix[tr][i]);
-            }tr++;
-            for(int i=tr; i<=br; i++){
-                list.add(matrix[i][rc]);
-            } rc--;
+        while(tr<=br && rc>=lc){
 
-            if(tr <=br){
-                for(int i=rc; i>=lc; i--){
-                list.add(matrix[br][i]);
-              } br--;
-            }
-               
+           for(int i=lc; i<=rc;i++){
+             list.add(matrix[tr][i]);
+           }tr++;
 
-            if(lc <= rc)  {
-                for(int i=br; i>=tr; i--){
-                list.add(matrix[i][lc]);
+           for(int i=tr; i<=br;i++){
+             list.add(matrix[i][rc]);
+           }rc--;
+
+           if(tr <= br){
+             for(int i=rc; i>=lc;i--){
+             list.add(matrix[br][i]);
+           }br--;
+           }
+
+           if(lc <= rc){
+             for(int i=br; i>=tr;i--){
+             list.add(matrix[i][lc]);
             }lc++;
-            } 
+           }
+
         }
 
         return list;
