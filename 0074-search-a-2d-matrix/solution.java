596bf2452c15cbf5beb5ba2c25cc248dac4459a5
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-04-15 04:49:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (42.18%)

--- 0074-search-a-2d-matrix/solution.java
+++ 0074-search-a-2d-matrix/solution.java
@@ -1,36 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    int row = matrix.length;
-    int col = matrix[0].length;
-    
-     int[] sin = new int[row*col];
-     
-     int k =0;
-     for(int i =0; i<row;i++){
-       for(int j =0; j<col; j++){
-         sin[k] = matrix[i][j];
-         k++;
-       }
-     }
-     
-     int low =0;
-     int high = sin.length -1;
-     boolean check = false;
-     while(low <= high){
-       int mid = (low+high)/2;
-       
-       if(sin[mid] == target){
-         check = true;
-         break;
-       }
-       else if(sin[mid] > target){
-         high = mid -1;
-       }else{
-         low = mid+1;
-       }
-     }
-     
-    return check;
-     
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i =0;
+        int j = col-1;      
+            while(i < row && j>=0){
+                    int a = matrix[i][j];
+
+                    if(a == target){
+                        return true;
+                    }
+                    else if(a > target){
+                        j--;
+                    }else{
+                        i++;
+                    }
+            }
+
+            return false;
     }
 }
