4c2fd5f26ecd2a1cc6a3aecb1a1f514d9d1dec0c
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-04-20 19:42:32 +0000

    [LeetCode Sync] Runtime - 2 ms (16.86%), Memory - 46.6 MB (28.62%)

---
+++ 0099-recover-binary-search-tree/solution.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> li = new ArrayList<>();
+    int p =Integer.MIN_VALUE;
+    int q =Integer.MIN_VALUE;
+    public List<Integer> inorder(TreeNode root){
+        if(root != null){
+            inorder(root.left);
+            li.add(root.val);
+            inorder(root.right);
+        }
+
+        return li;
+    }
+    
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        for(int i=0; i<li.size()-1;i++){
+          if(li.get(i) > li.get(i+1)){
+           if(p == Integer.MIN_VALUE){
+                    p = li.get(i);
+                    q = li.get(i+1);
+                } else {
+                    q = li.get(i+1);
+                }
+        }
+       }
+        swap(root); 
+    }
+    public  void swap(TreeNode root){
+        if(root != null){
+            if(root.val == p) {
+                root.val = q;
+            }else if(root.val == q){
+                root.val = p;
+            }
+
+            swap(root.left);
+            swap(root.right);
+        } 
+    }
+}
