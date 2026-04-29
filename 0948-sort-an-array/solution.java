93a14b3bf3f98425c1e976ed21eaf51ee1aa8735
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-04-29 08:05:26 +0000

    [LeetCode Sync] Runtime - 97 ms (12.92%), Memory - 80.1 MB (90.85%)

--- 0948-sort-an-array/solution.java
+++ 0948-sort-an-array/solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+         PriorityQueue<Integer> minh = new PriorityQueue<>();
+         int[] ans = new int[nums.length];      
+         for(int i = 0; i<nums.length; i++){
+            minh.offer(nums[i]);
+         }
+        
+        for(int i =0;i<nums.length;i++){
+            ans[i] = minh.poll();
+        }
+        
+        return ans;
+
     }
 }
