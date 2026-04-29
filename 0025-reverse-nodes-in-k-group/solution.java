9d246ec56fa906807afd49d575f7bef803a153ae
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-04-29 13:17:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.2 MB (88.52%)

---
+++ 0025-reverse-nodes-in-k-group/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode current = head;
+        int count = 0;
+
+        // Check if k nodes exist
+        while (current != null && count < k) {
+            current = current.next;
+            count++;
+        }
+
+        // Reverse only if k nodes available
+        if (count == k) {
+
+            current = reverseKGroup(current, k);
+
+            while (count-- > 0) {
+                ListNode next = head.next;
+                head.next = current;
+                current = head;
+                head = next;
+            }
+
+            head = current;
+        }
+
+        return head;
+    }
+}
