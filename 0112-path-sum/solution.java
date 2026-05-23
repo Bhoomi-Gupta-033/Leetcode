ac2ac82fca3330df5384729c8f97f04c8a16fad0
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-05-23 17:21:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.1 MB (14.00%)

--- 0112-path-sum/solution.java
+++ 0112-path-sum/solution.java
@@ -15,29 +15,12 @@
  */
 class Solution {
 
-    //  public static boolean divideTree(TreeNode p  , TreeNode q , int targetSum){
-    //     if(p == null && q == null) return 0;
-        
-    //     if(p.val != q.val) return false;
-
-    //     int  left = divideTree(p.left , q.right , targetSum);
-    //     int  right = divideTree(p.right , q.left , targetSum);
-
-    //     if(left == targetSum || right == targetSum) return true;
-    //     return false;
-    // }
-
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        // public boolean hasPathSum(TreeNode root, int targetSum) {
-    if (root == null) return false;
+     if(root == null) return false;
+     if(root.left == null && root.right == null){
+         return root.val == targetSum;
+     }
 
-    // leaf node
-    if (root.left == null && root.right == null) {
-        return targetSum == root.val;
-    }
-
-    return hasPathSum(root.left, targetSum - root.val) ||
-           hasPathSum(root.right, targetSum - root.val);
-          
+        return hasPathSum(root.left , targetSum - root.val) || hasPathSum(root.right , targetSum - root.val);   
     }
 }
