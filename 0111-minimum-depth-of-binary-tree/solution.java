5fe7619c2fdff98c97132324a8caba789908bea3
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-05-23 17:32:13 +0000

    [LeetCode Sync] Runtime - 4 ms (68.86%), Memory - 82 MB (53.81%)

--- 0111-minimum-depth-of-binary-tree/solution.java
+++ 0111-minimum-depth-of-binary-tree/solution.java
@@ -15,21 +15,17 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
+       if(root == null) return 0;
+       if(root.left == null && root.right == null) return 1;
 
-        if(root.left == null && root.right == null) return 1;
+       int left = minDepth(root.left);
+       int right = minDepth(root.right);
 
-        int  left = minDepth(root.left);
-        int right = minDepth(root.right);
-
-        // if (root.left == null) return minDepth(root.right) + 1;
-        // if (root.right == null) return minDepth(root.left) + 1;
-
-        if (left == 0 || right == 0) {
-            return left + right + 1;
-        }
-
-        return Math.min(left , right) +1;
+       if(left == 0 || right == 0){
+         return left + right + 1;
+       }
+       
+        return Math.min(left , right) + 1;  
         
-            }
+          }
 }
