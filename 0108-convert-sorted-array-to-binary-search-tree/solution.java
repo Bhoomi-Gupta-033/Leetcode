66387f524fca1fb0e583ee3a39dd907f945ef78c
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-05-23 17:57:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.8 MB (79.44%)

--- 0108-convert-sorted-array-to-binary-search-tree/solution.java
+++ 0108-convert-sorted-array-to-binary-search-tree/solution.java
@@ -1,54 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public TreeNode helper(int[] nums , int low , int high){
-         if(low > high){
-              return null;
-         }
+        if(low > high) return null;
 
-         int mid = (low + high)/2;
-         TreeNode root = new TreeNode(nums[mid]);
-         root.left = helper(nums , low , mid-1);
-         root.right = helper(nums , mid+1 , high);
+        int mid = (low + high)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums , low , mid-1);
+        root.right = helper(nums , mid+1 , high);
 
-         return root;
+        return root;
     }
+  
     public TreeNode sortedArrayToBST(int[] nums) {
-        // int low = 0;
-        // int high = nums.length -1;
-        // int middle= (low + high)/2;
-        //  TreeNode root = new TreeNode(nums[middle]);
-        //  low = middle +1;
-        // while(low <= high){
-        //     int mid = (low + high)/2;
-        //      root.right = new TreeNode(nums[mid]);
-        //     low = mid+1;
-        // }
-
-        // low = 0;
-        // high = middle -1;
-
-        // while(low <= high){
-        //     int mid = (low + high)/2;
-        //     root.left = new TreeNode(nums[mid]);
-        //    high = mid -1;
-        // }
-
-        // return root;
-
-        return helper(nums , 0 , nums.length-1);
+        
+       return helper(nums , 0 , nums.length -1);
     }
 }
