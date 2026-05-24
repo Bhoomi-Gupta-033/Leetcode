5789d4a0af1f27406af0dc77a60f0f509f4d9bff
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-05-24 11:17:35 +0000

    [LeetCode Sync] Runtime - 2 ms (16.86%), Memory - 46.7 MB (12.75%)

--- 0099-recover-binary-search-tree/solution.java
+++ 0099-recover-binary-search-tree/solution.java
@@ -15,42 +15,48 @@
  */
 class Solution {
     ArrayList<Integer> li = new ArrayList<>();
-    int p =Integer.MIN_VALUE;
-    int q =Integer.MIN_VALUE;
-    public List<Integer> inorder(TreeNode root){
+    int p = Integer.MIN_VALUE;
+    int q = Integer.MIN_VALUE;
+
+    public List<Integer> inOrder(TreeNode root){
         if(root != null){
-            inorder(root.left);
+            inOrder(root.left);
             li.add(root.val);
-            inorder(root.right);
+            inOrder(root.right);
         }
 
         return li;
     }
     
     public void recoverTree(TreeNode root) {
-        inorder(root);
-        for(int i=0; i<li.size()-1;i++){
-          if(li.get(i) > li.get(i+1)){
-           if(p == Integer.MIN_VALUE){
-                    p = li.get(i);
-                    q = li.get(i+1);
-                } else {
-                    q = li.get(i+1);
-                }
+        inOrder(root);
+
+        for(int i = 0; i<li.size()-1; i++){
+            if(li.get(i) > li.get(i+1)){
+               if(p == Integer.MIN_VALUE){
+                 p = li.get(i);
+                q= li.get(i+1);
+               }else{
+                q = li.get(i+1);
+               }
+            }
         }
-       }
-        swap(root); 
+          swap(root);
+
     }
-    public  void swap(TreeNode root){
+
+    public void swap(TreeNode root){
+        if(root == null) return;
+
         if(root != null){
-            if(root.val == p) {
+            if(root.val == p){
                 root.val = q;
             }else if(root.val == q){
                 root.val = p;
             }
 
-            swap(root.left);
-            swap(root.right);
-        } 
+        }
+        swap(root.left);
+        swap(root.right);
     }
 }
