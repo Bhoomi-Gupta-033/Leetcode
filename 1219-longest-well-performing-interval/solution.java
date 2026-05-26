9e3324e3a36238b87ab1b848176301eea338fb8a
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-05-26 16:50:57 +0000

    [LeetCode Sync] Runtime - 14 ms (38.05%), Memory - 46.8 MB (92.80%)

--- 1219-longest-well-performing-interval/solution.java
+++ 1219-longest-well-performing-interval/solution.java
@@ -1,23 +1,23 @@
 class Solution {
     public int longestWPI(int[] hours) {
          int score = 0;
-    Map<Integer, Integer> seen = new HashMap<>();
-    int ans = 0;
+         Map<Integer , Integer> seen = new HashMap<>();
+         int ans = 0;
 
-    for (int i = 0; i < hours.length; i++) {
-        score += hours[i] > 8 ? 1 : -1;
+         for(int i = 0; i<hours.length; i++){
+            score+= hours[i] > 8  ? 1: -1;
 
-        if (score > 0) {
-            ans = i + 1;
-        } else {
-            if (seen.containsKey(score - 1)) {
-                ans = Math.max(ans, i - seen.get(score - 1));
+            if(score > 0){
+                ans = i+1;
+            }else{
+                if(seen.containsKey(score-1)){
+                    ans = Math.max(ans , i - seen.get(score -1));
+                }
             }
-        }
 
-        seen.putIfAbsent(score, i);
-    }
+            seen.putIfAbsent(score , i);
+         }
 
-    return ans;
+         return ans;
     }
 }
