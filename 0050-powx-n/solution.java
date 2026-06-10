fcaf388836b17fe493e0cb6fd0b5c6c24d7fbaa9
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-06-10 08:30:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48 MB (46.20%)

--- 0050-powx-n/solution.java
+++ 0050-powx-n/solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-       return Math.pow(x,n);
+
+     return pow( x , (long)n );
+    }
+    public double pow(double x , long n){
+        if(x == 1) return 1;
+        if(n < 0){
+           return pow(1/x , -n);
+        }
+        if(n == 0) return 1;
+
+        if(n %2 == 0){
+            return pow(x*x , n/2);
+        }else{
+            return x * pow(x*x , (n-1)/2);
+        }
     }
 }
