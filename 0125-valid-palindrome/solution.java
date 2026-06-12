d70d059e43f31ceb8b6b0625e9928fa8e9c000a1
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-06-12 08:21:58 +0000

    [LeetCode Sync] Runtime - 16 ms (13.95%), Memory - 49.5 MB (5.07%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]", "");
+        str = str.toLowerCase();  
+        return check(str , 0 , str.length()-1);
+    }
+
+    public boolean check(String str , int low , int high){
+        if(low >= high) return true;
+        if(str.charAt(low) != str.charAt(high)) return false;
         
-        String str = s.replaceAll("[^a-zA-Z0-9]", ""); 
-        int n = str.length();
-        int low = 0;
-        int high = n-1;
-        str = str.toLowerCase();
-        while(low <=high){
-            if(str.charAt(low) != str.charAt(high)){
-                return false;
-            }
-            low++;
-            high--;
-
-        }
-
-        return true;
+          return check(str , low+1 , high-1);
     }
 }
