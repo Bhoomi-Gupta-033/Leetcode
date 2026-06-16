38e8b7b727dd68d660d3e92047b4acf99110c64f
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-06-16 08:43:16 +0000

    [LeetCode Sync] Runtime - 5 ms (8.43%), Memory - 45.7 MB (67.28%)

--- 0039-combination-sum/solution.java
+++ 0039-combination-sum/solution.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> finalList = new ArrayList<>();
-        List<Integer> currList = new ArrayList<>();
-
-        backtrack(finalList , currList , candidates , 0 ,0 , target);
+          Arrays.sort(candidates);
+        backtrack(finalList, new ArrayList<>(), candidates , 0 , target);
         return finalList;
-    } 
-    public static void backtrack(List<List<Integer>> finalList , List<Integer> currList , int[] nums , int start , int sum , int target){
 
-          if(sum == target){
+    }
+
+    public void backtrack(List<List<Integer>> finalList, List<Integer> currList, int[] nums, int start ,
+            int target) {
+           if(target < 0) return;
+        else if (target == 0) {
             finalList.add(new ArrayList<>(currList));
-          }
-
-        for (int i = start; i < nums.length; i++) {
-            if(sum < target){
-            currList.add(nums[i]);
-            backtrack(finalList, currList, nums, i , sum + nums[i] ,     target);
-            currList.remove(currList.size() - 1);
-            }   
+            // return;
         }
- }
+
+        else{
+           for (int i = start; i < nums.length; i++) {
+             
+             currList.add(nums[i]);
+             backtrack(finalList, currList , nums, i, target-nums[i]);
+             currList.remove(currList.size() - 1);
+            }
+        }
+    }
 }
