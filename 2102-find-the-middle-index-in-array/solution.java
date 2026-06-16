c20c44e6245434441548575a05ba7c9ab8163813
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-06-16 06:03:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (57.16%)

---
+++ 2102-find-the-middle-index-in-array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        int[] runningSum1 = new int[n]; 
+        int[] runningSum2 = new int[n]; 
+
+        runningSum1[0] = nums[0];
+        runningSum2[n-1] = nums[n-1];
+
+        for(int i = 1;i<n;i++){
+            runningSum1[i] = runningSum1[i-1] + nums[i];
+        }
+
+        for(int i =n-2;i>=0;i--){
+            runningSum2[i] = runningSum2[i+1] + nums[i];
+        }
+          
+        for(int i =0; i<n;i++){
+            if(runningSum1[i] == runningSum2[i]) return i;
+        }  
+         
+         return -1;
+    }
+}
