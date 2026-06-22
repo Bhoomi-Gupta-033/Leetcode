adb567b1057525824d4cee442f01a19228918982
Bhoomi-Gupta-033 <guptabhumi033@gmail.com>
2026-06-22 19:27:09 +0000

    [LeetCode Sync] Runtime - 19 ms (8.23%), Memory - 45.5 MB (16.79%)

--- 0415-add-strings/solution.java
+++ 0415-add-strings/solution.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-         BigInteger s1 = new BigInteger(num1);
+        BigInteger s1 = new BigInteger(num1);
          BigInteger s2 = new BigInteger(num2);
 
          BigInteger ans = s1.add(s2);
